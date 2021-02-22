6e1fa5879fbeddbeb08421f1c5e8ad249ab9bfd1
iqbal <iqbalitfadil@gmail.com>
2021-02-22 14:47:43 +0700

    first commit

--- App.js
+++ App.js
@@ -1,16 +1,46 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation';
+import PegawaiMain from './pegawai/PegawaiMain';
+import PegawaiRead from './pegawai/PegawaiRead';
+import PegawaiEdit from './pegawai/PegawaiEdit';
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+
+
+const RootStack = createStackNavigator(
+  {
+
+    PegawaiEdit: {
+      screen: PegawaiEdit,
+      navigationOptions: {
+
+      }
+    },    
+    PegawaiMain: {
+      screen: PegawaiMain,
+      navigationOptions: {
+
+      }
+    },
+
+    PegawaiRead: {
+      screen: PegawaiRead,
+      navigationOptions: {
+
+      }
+    },
+    
+  },
+  {
+    initialRouteName: 'PegawaiMain', // ini root
+  }
+);
+
+const AppContainer = createAppContainer(AppContainer);
+export default class App extends React.Component {
+
+  render() {
+    return <AppContainer />;
+  }
 }
+
